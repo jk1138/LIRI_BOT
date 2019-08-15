7c47563af68318183a1ed6c210dc8d1286c306f6
jyk <47995007+jk1138@users.noreply.github.com>
2019-08-15 15:37:34 -0400

    added console log with additional info so user has command choices

--- keys.js
+++ keys.js
@@ -1,4 +1,13 @@
-console.log('this is loaded');
+//Instead of a console.log that keys.js has been added to the liri.js file
+// I added directions for search terms and the inital console.log when loading 'node liri.js' in the terminal.
+console.log(
+  "\n Welcome! Here are some search options:" +
+  "\n 1) node liri.js concert-this <artist/band name here>" +
+  "\n 2) node liri.js spotify-this-song <song name here>" +
+  "\n 3) node liri.js movie-this <movie name here>"+
+  "\n 4) node liri.js do-what-it-says"
+
+);
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
